4007c579cb618eb84eb242a15412acc735cdb5ef
Jack Stevenson <cogwirrel@gmail.com>
2013-11-22 15:19:00 +0000

    Hello world, set up intellij

---
+++ src/com/chestday/squat/Squat.java
@@ -0,0 +1,15 @@
+package com.chestday.squat;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: js3509
+ * Date: 22/11/13
+ * Time: 15:14
+ * To change this template use File | Settings | File Templates.
+ */
+public class Squat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
