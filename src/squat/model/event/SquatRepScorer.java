d3c1525cfe0397946f04a0a3a88ae1cfcbc18c4f
Jack Stevenson <cogwirrel@gmail.com>
2014-04-28 15:00:25 +0100

    Penalise squats that don't go below parallel

--- src/squat/model/event/SquatRepScorer.java
+++ src/squat/model/event/SquatRepScorer.java
@@ -8,13 +8,16 @@ import squat.model.Model;
 public class SquatRepScorer {
 	private SquatScorer scorer;
 	private List<Double> scores;
+	private List<String> mainContributors;
 	
 	public SquatRepScorer(final ModelEventManager modelEventManager) {
 		scores = new ArrayList<Double>();
+		mainContributors = new ArrayList<String>();
 		scorer = new SquatScorer(modelEventManager);
 		modelEventManager.addListener(ModelEventType.SQUAT_DESCEND_START, new ModelEventListener() {
 			public void onEvent(Model m) {
 				scores.add(scorer.getCurrentScore());
+				mainContributors.add(scorer.getMainContributor());
 				scorer = new SquatScorer(modelEventManager);
 			}
 		});
@@ -24,7 +27,7 @@ public class SquatRepScorer {
 		int r = 0;
 		for(Double d : scores) {
 			r++;
-			System.out.println("Rep: " + r + " Score: " + d);
+			System.out.println("Rep: " + r + " Score: " + d + " Problem: " + mainContributors.get(r-1));
 		}
 		return scores;
 	}
--- src/squat/model/event/SquatScorer.java
+++ src/squat/model/event/SquatScorer.java
@@ -15,11 +15,19 @@ public class SquatScorer {
 	private static final String KNEES_FORWARD = "Knees Forward";
 	private static final String KNEES_BACKWARD = "Knees Backward";
 	private static final String FOOT_PLACEMENT = "Foot Placement";
+	private static final String ABOVE_PARALLEL = "Above Parallel";
+	
+	private static final double DEPTH_PENALTY = 0.30; // As a percentage
 	
 	private BadFrameCounter badFrameCounter;
 	private FrameCounter frameCounter;
+	private ParallelChecker parallelChecker;
+	
 	private ModelEventManager modelEventManager;
 	
+	private boolean didSquatBelowParallel = false;
+	
+	// This class is to be used to evaluate a single rep.
 	public SquatScorer(ModelEventManager modelEventManager) {
 		this.modelEventManager = modelEventManager;
 		
@@ -27,10 +35,11 @@ public class SquatScorer {
 		
 		badFrameCounter = new BadFrameCounter();
 		frameCounter = new FrameCounter();
+		parallelChecker = new ParallelChecker();
 		
 		modelEventManager.addListener(ModelEventType.SQUAT_BAD_FORM, badFrameCounter);
-		
 		modelEventManager.addListener(ModelEventType.TICK, frameCounter);
+		modelEventManager.addListener(ModelEventType.SQUAT_BELOW_PARALLEL_START, parallelChecker);
 	}
 	
 	private void initialiseContributors() {
@@ -40,11 +49,11 @@ public class SquatScorer {
 		contributors.put(KNEES_FORWARD, 0);
 		contributors.put(KNEES_BACKWARD, 0);
 		contributors.put(FOOT_PLACEMENT, 0);
+		contributors.put(ABOVE_PARALLEL, 0);
 	}
 	
 	private class BadFrameCounter implements ModelEventListener {
 		public void onEvent(Model m) {
-			badFrameCount++;
 			
 			if(!m.isSquatWeightOverFeet()) {
 				contributors.put(WEIGHT_DISTRIBUTION, contributors.get(WEIGHT_DISTRIBUTION) + 1);
@@ -66,12 +75,35 @@ public class SquatScorer {
 		}
 	}
 	
+	private class ParallelChecker implements ModelEventListener {
+		public void onEvent(Model m) {
+			didSquatBelowParallel = true;
+		}
+	}
+	
 	public double getCurrentScore() {
 		if(frameCount == 0) {
 			return 0;
 		}
-				
-		return (1 - ((double)badFrameCount / (double)frameCount)) * 100;
+		
+		if(!didSquatBelowParallel) {
+			contributors.put(ABOVE_PARALLEL, (int)(frameCount * DEPTH_PENALTY));
+		}
+		
+		double score = (1 - ((double)badFrameCount() / (double)frameCount)) * 100;
+		
+		// Remove negative scores!
+		score = Math.max(0, score);
+		
+		return score;
+	}
+	
+	private int badFrameCount() {
+		int count = 0;
+		for(Integer i : contributors.values()) {
+			count += i;
+		}
+		return count;
 	}
 	
 	public Map<String, Integer> getContributors() {
@@ -79,6 +111,7 @@ public class SquatScorer {
 	}
 	
 	public String getMainContributor() {
+		
 		int maxContribution = 0;
 		String mainContributor = "Perfect Squat";
 		for (Map.Entry<String, Integer> entry : contributors.entrySet()) {
@@ -93,5 +126,6 @@ public class SquatScorer {
 	public void shutdown() {
 		modelEventManager.removeListener(ModelEventType.SQUAT_BAD_FORM, badFrameCounter);
 		modelEventManager.removeListener(ModelEventType.TICK, frameCounter);
+		modelEventManager.removeListener(ModelEventType.SQUAT_BELOW_PARALLEL_START, parallelChecker);
 	}
 }
