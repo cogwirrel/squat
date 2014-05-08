c5b04811215761eaa1b2f784c922ca35cf83ac6c
Jack Stevenson <cogwirrel@gmail.com>
2014-05-08 12:53:05 +0100

    Refactor main into SquatPipeline

--- src/squat/Squat.java
+++ src/squat/Squat.java
@@ -32,138 +32,14 @@ public class Squat {
 
 	static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 	
-	private static final int INIT_FITTING_ITERATIONS = 3;
-	
+
 	public static void main(String[] args) throws Exception {
 		
 		VideoInput videoInput = new VideoInput("/home/jack/squat_vids/stable/good_squats.avi", true);
+		VideoDisplay videoDisplay = new VideoDisplay("Display", videoInput.getWidth(), videoInput.getHeight());
 		
-		int width = videoInput.getWidth();
-		int height = videoInput.getHeight();
-		
-		VideoDisplay videoDisplay = new VideoDisplay("Skeleton", width, height);
-		VideoDisplay videoDisplay2 = new VideoDisplay("Model Fit", width, height);
-		
-		final Scalar modelColour = new Scalar(255,255,255);
-		
-		final ModelEventManager modelEventManager = new ModelEventManager();
-		
-		modelEventManager.addListener(ModelEventType.SQUAT_BELOW_PARALLEL_START, new ModelEventListener() {
-			public void onEvent(Model m) {
-				modelColour.set(new double[]{0, 255, 0});
-			}
-		});
-		
-		modelEventManager.addListener(ModelEventType.SQUAT_BELOW_PARALLEL_END, new ModelEventListener() {
-			public void onEvent(Model m) {
-				modelColour.set(new double[]{255, 255, 255});
-			}
-		});
-		
-		modelEventManager.addListener(ModelEventType.SQUAT_LOCKOUT_START, new ModelEventListener() {
-			public void onEvent(Model m) {
-				modelColour.set(new double[]{0, 255, 0});
-			}
-		});
-		
-		modelEventManager.addListener(ModelEventType.SQUAT_LOCKOUT_END, new ModelEventListener() {
-			public void onEvent(Model m) {
-				modelColour.set(new double[]{255, 255, 255});
-			}
-		});
-		
-		final SquatRepScorer squatScorer = new SquatRepScorer(modelEventManager);
-		final SquatRepCounter sqrc = new SquatRepCounter(modelEventManager);
-			
-		Mat firstFrame = new Mat();
-		if(videoInput.hasNextFrame()) {
-			firstFrame = videoInput.getNextFrame();
-		}
-		
-		BackgroundSubtractor bg = new BackgroundSubtractorNaive(firstFrame, 30);
-		
-		SquatSetup squatSetup = new SquatSetup(bg, firstFrame);
-		Mat readyFrame = new Mat();
-		while(!squatSetup.ready() && videoInput.hasNextFrame()) {
-			readyFrame = videoInput.getNextFrame();
-			videoDisplay.show(readyFrame);
-			videoDisplay.draw();
-			squatSetup.update(readyFrame);
-		}
-		
-		System.out.println("Ready to squat!");
-		
-		// We have got to the point where the lifter is ready to squat
-		
-		// Find where to put the model and how large to make it
-		
-		Mat readyForeground = bg.subtract(readyFrame);
-		// Calculate the height and centre point of the foreground blob - ie. the figure
-		MatOfPoint figureContours = VideoTools.largestObject(readyForeground);
-		Rect figureBound = Imgproc.boundingRect(figureContours);
-		int figureHeight = figureBound.height;
-		System.out.println("Figure Height: " + figureHeight);
-		Point figureCentre = new Point(figureBound.x + figureBound.width / 2, figureBound.y + figureBound.height / 2);
-		System.out.println("Figure x: " + figureCentre.x + " y: " + figureCentre.y);
-		
-		// Initialise the model in a sensible location
-		AngularModel model = new AngularModel(figureCentre.x - figureBound.width / 2, figureCentre.y + figureHeight / 2);
-		Mat modelScaleMat = new Mat(readyForeground.size(), readyForeground.type());
-		model.draw(modelScaleMat);
-		
-		// Calculate the height of the model
-		MatOfPoint modelContours = VideoTools.largestObject(modelScaleMat);
-		Rect modelBound = Imgproc.boundingRect(modelContours);
-		int modelHeight = modelBound.height;
-		
-		System.out.println("Model Height: " + modelHeight);
-		
-		// Calculate the scaling factor to best fit the model to the figure
-		double scale = (double)figureHeight / (double)modelHeight;
-		model.setScale(scale);
-		
-		System.out.println("SCALE: " + scale);
-		
-		ModelFitter fitter = new ModelFitterOptim();
-		//ModelFitter fitter = new ModelFitterManual(width, height);
-		
-		// Initial fitting!!
-		ModelFitter initFit = new ModelInitialisationFitterOptim();
-		
-		Mat frm = new Mat();
-		if(videoInput.hasNextFrame()) {
-			frm = videoInput.getNextFrame();
-		}
-		
-		for(int i = 0; i < INIT_FITTING_ITERATIONS; i++) {
-			initFit.fit(model, bg.subtract(frm));
-		}
-		
-		// We have the initial model fitted
-		// Start the main squat analysis
-		SquatTracker squatTracker = new SquatTracker(model, modelEventManager, bg);
-		squatTracker.start();
-		
-		// Main loop
-		while(videoInput.hasNextFrame() && !squatTracker.finished()) {
-			Mat frame = videoInput.getNextFrame();
-			
-			squatTracker.update(frame);
-			
-			Mat m = new Mat(frame.size(), frame.type());
-			Mat m2 = new Mat(frame.size(), frame.type());
-			model.draw(m2, modelColour);
-			model.drawSkeleton(m, modelColour);
-			
-			videoDisplay.show(VideoTools.blend(frame, m));
-			videoDisplay.draw();
-			
-			videoDisplay2.show(VideoTools.blend(frame, m2));
-			videoDisplay2.draw();
-			
-			//System.out.println(frameNumber);
-			//frameNumber++;
-		}
+		SquatPipeline squatPipeline = new SquatPipeline(videoInput, videoDisplay);
+		squatPipeline.process();
 		
 		// Cycle through the last few frames
 		while(videoInput.hasNextFrame()) {
@@ -173,12 +49,8 @@ public class Squat {
 		}
 		
 		videoDisplay.close();
-		videoDisplay2.close();
 		
 		System.out.println("done");
-		System.out.println("Reps: " + squatTracker.getReps());
-		System.out.println("Score: " + squatTracker.getScores());
-
 	}
 
 }
