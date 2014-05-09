612e57c1925f57d9ffe282b0eb77a65707b192ac
Jack Stevenson <cogwirrel@gmail.com>
2014-05-09 17:17:35 +0100

    Refactor motion (stillness) detection into MotionDetector class

--- src/squat/tracking/SquatSetup.java
+++ src/squat/tracking/SquatSetup.java
@@ -5,51 +5,29 @@ import org.opencv.core.Mat;
 import squat.utils.BackgroundSubtractor;
 import squat.utils.FigureDetector;
 import squat.utils.FixedQueue;
+import squat.utils.MotionDetector;
 import squat.utils.VideoTools;
 
 public class SquatSetup {
-	private static final int FRAME_SKIP = 3;
-	private static final int NUM_DIFFERENCES = 7;
-	private static final double MOTION_THRESHOLD = 1;
 	
 	private BackgroundSubtractor bg;
 	private FigureDetector figureDetector;
-	private FixedQueue<Double> differences;
-	private Mat prevForeground;
-	private int frameCount = 0;
+	private MotionDetector motionDetector;
+
 	private boolean ready = false;
 	
 	public SquatSetup(BackgroundSubtractor backgroundSubtractor, Mat initialFrame) {
 		bg = backgroundSubtractor;
-		differences = new FixedQueue<Double>(NUM_DIFFERENCES);
+		motionDetector = new MotionDetector(bg.subtract(initialFrame));
 		figureDetector = new FigureDetector();
-		prevForeground = bg.subtract(initialFrame);
 	}
 	
 	public void update(Mat frame) {
 		Mat foreground = bg.subtract(frame);
 		
-		if(figureDetector.hasFigure(foreground) && frameCount % FRAME_SKIP == 0) {
-			int pixelDifference = VideoTools.countDifference(foreground, prevForeground);
-			double difference = 100 * (double)pixelDifference / (double)(frame.cols() * frame.rows());
-			differences.add(difference);
-			
-			System.out.println(difference);
-			
-			ready = differencesBelowThreshold();
-			
-			prevForeground = foreground;
+		if(figureDetector.hasFigure(foreground)) {
+			ready = motionDetector.stationary(foreground);
 		}
-		
-		frameCount++;
-	}
-	
-	private boolean differencesBelowThreshold() {
-		boolean belowThreshold = differences.size() == NUM_DIFFERENCES;
-		for(Double d : differences.getList()) {
-			belowThreshold &= d < MOTION_THRESHOLD;
-		}
-		return belowThreshold;
 	}
 	
 	public boolean ready() {
